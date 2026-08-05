87dacb9673488c4c1d25fd20f27b1b2d62fb2542
krithyasrivastava <krithyasrivastava@gmail.com>
2026-08-05 22:00:31 +0530

    first commit

---
+++ day 1/app.js
@@ -0,0 +1,15 @@
+// const fs = require('fs')
+// const data = fs.readFileSync('notes.txt','utf-8')
+// console.log(data)
+//fs.writeFileSync('notes.txt','My Name Is Aditya')//
+
+// const path = require('path');
+// const filePath = path.join(__dirname, 'test','hello.text');
+// console.log(filePath);
+
+// const os= require('os');
+// const user = os.userInfo();
+// console.log(user);
+
+const des = require('./data/des');
+console.log(des);
