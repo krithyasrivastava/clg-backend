d8210ee1d49fb6a48f976713ce02d59ee4ba9215
Krithya Srivastava <krithyasrivastava@gmail.com>
2026-09-05 18:06:27 +0530

    Modify notes file and log process environment variable
    Updated the name in the notes file and logged the process environment variable for PORT.

--- web-dev-3/day2/app.js
+++ web-dev-3/day2/app.js
@@ -1,61 +1,46 @@
 <<<<<<< HEAD
-console.log("hello,World"); 
-console.log("hello,World"); 
+// const fs = require('fs');
+// const data = fs.readFileSync('notes.txt', 'utf-8');
+// console.log(data);
+// fs.writeFileSync('notes.txt', 'Krithya');
 
-const fs = require('fs');
-const data = fs.readFileSync('notes.txt', 'utf-8')
-console.log(data)
-fs.writeFileSync('notes.txt','My name is mukul');
+// const path = require('path');
+// const filepath = path.join(__dirname,'test', 'hello.txt');
+// console.log(filepath);
 
-const path = require('path');
-const filepath = path.join(__dirname, 'test','hello.txt');
-console.log(filepath) 
+// const os = require('os');
+// const user = os.userInfo();
+// console.log(user);
 
-const os= require('os');
-const user = os.userInfo();
+// const des = require('./data/des');
+// console.log(des);
 
-console.log(user)
+// const package = require('./data/package');
+// console.log(package);   
 
-const des = require('./module/des');
-console.log(des); 
-
-const packages = require('./module/packages');
-console.log(packages);   
-
-
-
-
-
-
-
+require('dotenv').config();
+const process = require('process');
 =======
-console.log("hello,World"); 
-console.log("hello,World"); 
+// const fs = require('fs');
+// const data = fs.readFileSync('notes.txt', 'utf-8');
+// console.log(data);
+// fs.writeFileSync('notes.txt', 'Krithya');
 
-const fs = require('fs');
-const data = fs.readFileSync('notes.txt', 'utf-8')
-console.log(data)
-fs.writeFileSync('notes.txt','My name is Nitin');
+// const path = require('path');
+// const filepath = path.join(__dirname,'test', 'hello.txt');
+// console.log(filepath);
 
-const path = require('path');
-const filepath = path.join(__dirname, 'test','hello.txt');
-console.log(filepath) 
+// const os = require('os');
+// const user = os.userInfo();
+// console.log(user);
 
-const os= require('os');
-const user = os.userInfo();
+// const des = require('./data/des');
+// console.log(des);
 
-console.log(user)
+// const package = require('./data/package');
+// console.log(package);   
 
-const des = require('./module/des');
-console.log(des); 
-
-const packages = require('./module/packages');
-console.log(packages);   
-
-
-
-
-
-
-
+require('dotenv').config();
+const process = require('process');
 >>>>>>> 47935ed (Add Web Dev III)
+console.log(process.env.PORT);
